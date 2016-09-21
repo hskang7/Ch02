395a879c360cd4aa947f12c379ffa6a97bd1c4d7
hskang7 <hskang7@gmail.com>
2016-09-21 14:30:34 +0900

    C Project

---
+++ 1st Prj/first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	puts("인코딩 방식");
+	printf("\n");
+
+	puts("파일 / 고급저장 옵션");
+	puts("기본: 한국어 - 코드 페이지 949");
+	puts("수정: 유니코드(서명있는 UTF-8) - 코드 페이지 65001");
+
+	return 0;
+}
